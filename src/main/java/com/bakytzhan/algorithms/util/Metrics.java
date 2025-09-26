0636cdc4f52fe5815abf9ed3e3fd259b2538e0bc
Bakytzhan Kassymgali <kereyb01@gmail.com>
2025-09-26 17:18:18 +0500

    fix(metrics): add incrementComparisons for CI

--- src/main/java/com/bakytzhan/algorithms/util/Metrics.java
+++ src/main/java/com/bakytzhan/algorithms/util/Metrics.java
@@ -2,39 +2,50 @@ package com.bakytzhan.algorithms.util;
 
 public class Metrics {
     private long comparisons = 0;
-    private long allocations = 0;
     private int maxDepth = 0;
+    private long allocations = 0;
+
+
+    public void incrementComparisons() {
+        comparisons++;
+    }
+
 
     public int compare(int a, int b) {
         comparisons++;
         return Integer.compare(a, b);
     }
 
-    public void trackAllocation() {
-        allocations++;
-    }
-
-    public void updateDepth(int depth) {
+    public void recordDepth(int depth) {
         if (depth > maxDepth) {
             maxDepth = depth;
         }
     }
 
-    public long getComparisons() {
-        return comparisons;
+    public void updateDepth(int depth) {
+        recordDepth(depth);
     }
 
-    public long getAllocations() {
-        return allocations;
+    public void trackAllocation() {
+        allocations++;
+    }
+
+
+    public long getComparisons() {
+        return comparisons;
     }
 
     public int getMaxDepth() {
         return maxDepth;
     }
 
+    public long getAllocations() {
+        return allocations;
+    }
+
     public void reset() {
         comparisons = 0;
-        allocations = 0;
         maxDepth = 0;
+        allocations = 0;
     }
 }
